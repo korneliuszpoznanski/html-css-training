1bd362a53c47219c1b48cd2912559932284182db
korneliuszpoznanski <k.poznanski@gmail.com>
2023-03-23 23:47:47 +0100

    working

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,27 +1,36 @@
-function devide(){
-    let a, b;
-    let text;
+function random(){
+    var a;
+    var b = '';
+    a = Math.floor(Math.random() * 11);
+    b += a.toString();
+    return b;
     
-    a = document.getElementById('ida').value;
-    b = document.getElementById('idb').value;
+}
+    document.getElementById("ida").value = random();
+    document.getElementById("idb").value = random();
 
-    a = parseFloat(a);
-    b = parseFloat(b);
+function newValue(){
+    document.getElementById("ida").value = random();
+    document.getElementById("idb").value = random();
+    document.getElementById("idc").value = '';
+    document.getElementById("ekran").innerHTML = "";
+}
 
-    text = (a/b);
-    document.getElementById("ekran").innerHTML = text;
-};
+function check(){
+    let a,b,c;
+    let ekran;
+    a = document.getElementById("ida").value ;
+    b = document.getElementById("idb").value ;
+    c = document.getElementById("idc").value ;
+    a = parseInt(a);
+    b = parseInt(b);
+    c = parseInt(c);
 
-function multiply(){
-    let a, b;
-    let text;
-    
-    a = document.getElementById('ida').value;
-    b = document.getElementById('idb').value;
-
-    a = parseFloat(a);
-    b = parseFloat(b);
-
-    text = (a*b);
-    document.getElementById("ekran").innerHTML = text;
+        if (c == a*b) {
+            ekran = "BRAWO"
+            document.getElementById("ekran").innerHTML = ekran;
+        } else {
+            ekran = "FAŁSZ";
+            document.getElementById("ekran").innerHTML = ekran;
+        }
 }
