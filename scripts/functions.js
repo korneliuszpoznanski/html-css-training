f261743b6189213e3868b2914c58b085c2d31a8b
Korneliusz Poznanski <k.poznanski@gmail.com>
2023-03-23 10:01:52 +0100

    committed

---
+++ scripts/functions.js
@@ -0,0 +1,27 @@
+function devide(){
+    let a, b;
+    let text;
+    
+    a = document.getElementById('ida').value;
+    b = document.getElementById('idb').value;
+
+    a = parseFloat(a);
+    b = parseFloat(b);
+
+    text = (a/b);
+    document.getElementById("ekran").innerHTML = text;
+};
+
+function multiply(){
+    let a, b;
+    let text;
+    
+    a = document.getElementById('ida').value;
+    b = document.getElementById('idb').value;
+
+    a = parseFloat(a);
+    b = parseFloat(b);
+
+    text = (a*b);
+    document.getElementById("ekran").innerHTML = text;
+}
